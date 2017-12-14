72c88be972a381fa3db38a07f5ec5766583017e0
Mohammed Helwani <mhd_helwani@hotmail.com>
2017-12-14 10:58:27 +0100

    Version++ Write Uni email

--- plugin.php
+++ plugin.php
@@ -13,6 +13,6 @@ $ilias_max_version = "5.1.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Mohammed Helwani";
-$responsible_mail = "mhd_helwani@hotmail.com";
+$responsible_mail = "mohammed.helwani@llz.uni-halle.de";
  
 ?>
